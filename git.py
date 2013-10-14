7afabdaf6dfc816a945736321fa1d84564723697
Christoffer Rosen <cbr4830@rit.edu>
2013-10-14 14:19:55 -0400

    Remove all backslashes from git log and change the delimiters between each commit and between each git properties

--- git.py
+++ git.py
@@ -7,7 +7,7 @@ description: Holds the repository git abstraction class
 import os
 import subprocess
 import json
-import re
+
 class Git():
     """
     Git():
@@ -17,26 +17,26 @@ class Git():
     """
     # Two backslashes to allow one backslash to be passed in the command.
     LOG_CMD = 'git log --pretty=format:\"{\
-    \\"commit_hash\\": \\"%H\\",\
-    \\"commit_hash_abbreviated\\": \\"%h\\",\
-    \\"tree_hash\\": \\"%T\\",\
-    \\"tree_hash_abbreviated\\": \\"%t\\",\
-    \\"parent_hashes\\": \\"%P\\",\
-    \\"parent_hashes_abbreviated\\": \\"%p\\",\
-    \\"author_name\\": \\"%an\\",\
-    \\"author_email\\": \\"%ae\\",\
-    \\"author_date\\": \\"%ad\\",\
-    \\"author_date_rfc2822_style\\": \\"%aD\\",\
-    \\"author_date_relative\\": \\"%ar\\",\
-    \\"author_date_unix_timestamp\\": \\"%at\\",\
-    \\"author_date_iso_8601\\": \\"%ai\\",\
-    \\"committer_name\\": \\"%cn\\",\
-    \\"committer_email\\": \\"%ce\\",\
-    \\"committer_date\\": \\"%cd\\",\
-    \\"committer_date_relative\\": \\"%cr\\",\
-    \\"commit_message\\": \\"%b\\",\
-    \\"subject\\": \\"%s\\"\
-    },\"'
+    \\"commit_hash\\"CAS_READER_PROP_DELIMITER: \\"%H\\",CAS_READER_PROP_DELIMITER2\
+    \\"commit_hash_abbreviated\\"CAS_READER_PROP_DELIMITER: \\"%h\\",CAS_READER_PROP_DELIMITER2\
+    \\"tree_hash\\"CAS_READER_PROP_DELIMITER: \\"%T\\",CAS_READER_PROP_DELIMITER2\
+    \\"tree_hash_abbreviated\\"CAS_READER_PROP_DELIMITER: \\"%t\\",CAS_READER_PROP_DELIMITER2\
+    \\"parent_hashes\\"CAS_READER_PROP_DELIMITER: \\"%P\\",CAS_READER_PROP_DELIMITER2\
+    \\"parent_hashes_abbreviated\\"CAS_READER_PROP_DELIMITER: \\"%p\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_name\\"CAS_READER_PROP_DELIMITER: \\"%an\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_email\\"CAS_READER_PROP_DELIMITER: \\"%ae\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_date\\"CAS_READER_PROP_DELIMITER: \\"%ad\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_date_rfc2822_style\\"CAS_READER_PROP_DELIMITER: \\"%aD\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_date_relative\\"CAS_READER_PROP_DELIMITER: \\"%ar\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_date_unix_timestamp\\"CAS_READER_PROP_DELIMITER: \\"%at\\",CAS_READER_PROP_DELIMITER2\
+    \\"author_date_iso_8601\\"CAS_READER_PROP_DELIMITER: \\"%ai\\",CAS_READER_PROP_DELIMITER2\
+    \\"committer_name\\"CAS_READER_PROP_DELIMITER: \\"%cn\\",CAS_READER_PROP_DELIMITER2\
+    \\"committer_email\\"CAS_READER_PROP_DELIMITER: \\"%ce\\",CAS_READER_PROP_DELIMITER2\
+    \\"committer_date\\"CAS_READER_PROP_DELIMITER: \\"%cd\\",CAS_READER_PROP_DELIMITER2\
+    \\"committer_date_relative\\"CAS_READER_PROP_DELIMITER: \\"%cr\\",CAS_READER_PROP_DELIMITER2\
+    \\"commit_message\\"CAS_READER_PROP_DELIMITER: \\"%b\\",CAS_READER_PROP_DELIMITER2\
+    \\"subject\\"CAS_READER_PROP_DELIMITER: \\"%s\\"\
+    }CAS_READER_JSON_DELIMITER,\"'
 
     def log(self):
         """
@@ -55,31 +55,27 @@ class Git():
         # List of json objects
         json_list = []
 
-        # Serialize each object to JSON format
-        commitList = log.split("},{")
+        # JSON won't be able to read as is - clean each commit and append to json_list
+        commitList = log.split("}CAS_READER_JSON_DELIMITER,{")
         for commit in commitList:
-            #commit = commit.replace('    ','')
+          
             # Remove invalid json escape characters
             commit = commit.replace('\\x', '\\u00')
 
             # Remove quotes in JSON properties
-            propValues = commit.split(',    "')
+            propValues = commit.split(',CAS_READER_PROP_DELIMITER2    "')
             commitFixed = ""
             for propValue in propValues:
-                props = propValue.split('": "')
+                props = propValue.split('"CAS_READER_PROP_DELIMITER: "')
                 propStr = ''
                 for prop in props:
+                    prop = prop.replace('\\','') # json doesn't like escapes..
                     propStr = propStr + '"' + prop.replace('"','') + '":'
               
                 commitFixed = commitFixed + "," + propStr[0:-1]
      
          
             commitFixed = commitFixed[1:].replace('    ','')
-
-            # Handle all other invalid escape sequences
-            regex = re.compile(r'\\(?![/u"])')
-            commitFixed = regex.sub(r"\\\\",commitFixed)
-
             json_list.append(json.loads('{' + commitFixed + '}'))
     
         return json_list
