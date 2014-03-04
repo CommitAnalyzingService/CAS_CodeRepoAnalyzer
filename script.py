9a4ce772e5e478d1a2da352de6c31e2b75a5f7b0
Christoffer Rosen <cbr4830@rit.edu>
2014-03-03 21:49:05 -0500

    adding the user table and feedback table

--- script.py
+++ script.py
@@ -2,9 +2,11 @@
 file: script.py
 author: Christoffer Rosen <cbr4830@rit.edu>, Ben Grawi <bjg1568@rit.edu>
 date: Jan. 2014
-description: base script to call. 
+description: base script to call.
 """
 from cas_manager import *
+from orm.feedback import *
+from orm.user import *
 
 if len(sys.argv) > 1:
 	arg = sys.argv[1]
