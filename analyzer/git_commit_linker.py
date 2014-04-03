3c87a1afc1185cd4a97bbe78596ae5ed372e2704
Christoffer Rosen <cbr4830@rit.edu>
2014-04-03 12:18:15 -0400

    refactored the git linker to only use lines of code that was modified -- not added, when linking bug fixing changes to bug introducing changes.

--- analyzer/git_commit_linker.py
+++ analyzer/git_commit_linker.py
@@ -55,12 +55,12 @@ class GitCommitLinker:
     logging.info("#### marking and linking the buggy commits ####")
 
     for commit in all_commits:
-      logging.info(commit.commit_hash)
+      #logging.info(commit.commit_hash)
 
       if commit.commit_hash in linked_commits:
-        logging.info("marking..")
+      #  logging.info("marking..")
         commit.contains_bug = True
-        logging.info("fixes = " + str(linked_commits[commit.commit_hash]))
+       # logging.info("fixes = " + str(linked_commits[commit.commit_hash]))
         commit.fixes = str(linked_commits[commit.commit_hash])
 
 
@@ -76,7 +76,7 @@ class GitCommitLinker:
 
     region_chunks = self.getModifiedRegions(commit)
 
-    logging.info("Linkage: ")
+    logging.info("Linkage for commit " + commit.commit_hash)
     for k,v in region_chunks.items():
       logging.info("-- file: " + k)
       logging.info("---- regions: " + str(v))
@@ -84,87 +84,76 @@ class GitCommitLinker:
     bug_introducing_changes = self.gitAnnotate(region_chunks, commit)
     return bug_introducing_changes
 
-  def _getModifiedRegions(self, diff_cmd, files_modified):
+  def _getModifiedRegionsOnly(self, diff, files_modified):
     """
     returns a dict of file -> list of line numbers modified. helper function for getModifiedRegions
-    heavily commented as it may seem a bit janky. git diff doesn't provide a clean way of simply
-    getting the specific lines that were modified, so we are doing so here manually using grep. A possible
-    refactor in the future may be to use an external diff tool, so that this implementation wouldn't be scm
-    specific.
+    git diff doesn't provide a clean way of simply getting the specific lines that were modified, so we are doing so here 
+    manually using grep. A possible refactor in the future may be to use an external diff tool, so that this implementation 
+    wouldn't be scm (git) specific.
 
     if a file was merely deleted, then there was no chunk or region changed but we do capture the file.
     however, we do not assume this is a location of a bug.
+
+    modified means modified or deleted -- not added! We assume are lines of code modified is the location of a bug.
     """
-    logging.info("Getting modified regions..")
+   # logging.info("Getting modified regions go 2..")
     region_diff = {}
 
-    # initialize the dictionary with the files -> lists
     for file in files_modified:
 
       if file != "'" and file != "":
         region_diff[file] = []
 
-    # grep for the file and line number changed by looking for the '@@' and also
-    # grepping the line above it, which if this is a new file will show us this.
-    # we see that its so by checking if it matches one of the files modified, which is passed
-    # in to the function.
-    grep_cmd = "grep '@@' -B 1"
+    # split all the different regions 
+    regions = diff.split("diff --git")[1:] # remove the clutter
+   # logging.info("regions: " + str(regions))
 
-    # this is just dividing each region up in a very unfiltered manner
-    # example on how a region may look: "b'+++ b/{filename}\\n@@ {lines_modified_info} @@ {class}"
-    modified_regions = str( subprocess.check_output( diff_cmd + " | " + grep_cmd, shell=True ) ).split("--")
+    for region in regions:
+      #logging.info("region: " + region)
+      chunks = region.split("@@") # see git diff documentation 
 
-    # keep track of the last modified file, as if there are multiple regions modified within a file
-    # it will not list the same file again in the line before the modified liens of code.
-    last_modified_file = None
+      # if a binary file it doesn't display the lines modified (a.k.a the '@@' part)
+      if len(chunks) == 1:
+        continue
 
-    for region in modified_regions:
+      file_info = chunks[0] # file info is the first 'chunk', followed by the sections of code modified
 
-      # splits the information into three chuncks: line before the grep that may contain
-      # the file information, the place containing the lines of code modified, and the third
-      # chunck we don't care for.
-      region_info = region.split("@@")
+      # start with extracting the file name
+      name_start_index = file_info.find("b/")
+      file_name = file_info[name_start_index+2:]
+      name_end_index = file_name.find("\\n")
+      file_name = file_name[:name_end_index]
 
-      # if we arrived at a new file, we will see it here; otherwise, it will just contain
-      # un-interesting information grepped before the lines modified information.
-      possible_file_info = region_info[0]
-      lines_modified_info = region_info[1]
+      # it is possible there is a binary file being tracked or something we shouldn't care about  
+      if file_name == None or file_name not in region_diff:
+        continue
 
-      # logging.info("lines modified info: " + lines_modified_info)
+      # iterate through the following chunks to extract the section of code modified. 
+      for chunk in range(1, len(chunks), 2):
+        mod_line_info = chunks[chunk].split(" ")[1:-1][0] # remove clutter
+        mod_code_info = chunks[chunk+1].split("\\n")[1:-1] # remove clutter
 
-      # check to see if this is a file name -> if so, replace the last_modified_file
-      start_index = possible_file_info.find("b/")
+        # logging.info("File name: " + file_name)
+        # logging.info("mod_line-info: " + mod_line_info)
 
-      if start_index != -1:
-        possible_file_info = possible_file_info[start_index+2:] # move passed b/
+        # remove comma from mod_line_info as we only care about the start of the modification
+        if mod_line_info.find(",") != -1:
+          mod_line_info = mod_line_info[0:mod_line_info.find(",")]
 
-        # remove newline at end if existent
-        end_index = possible_file_info.find("\\n")
-        if end_index != -1:
-          possible_file_info = possible_file_info[:end_index]
+        current_line = int(mod_line_info)
 
-        #logging.info("verifying that possible file: " + possible_file_info + " is acceptable.")
+        # now only use the code line changes that MODIFIES (not adds) in the diff
+        for section in mod_code_info:
+        #  logging.info("section: " + section)
+          if section[0] == "-":
+          #  logging.info("modified @ line " + str(current_line) + " at file " + file_name)
+            # this lines modifies or deletes a line of code
+            region_diff[file_name].append(str(current_line)[1:]) # remove the negative sign.
 
-        # verify that this is indeed a valid file and not just something random grepped.
-        if possible_file_info in region_diff:
-        #  logging.info("verified")
-          last_modified_file = possible_file_info # set it as the last modified file.
-
-      # it is possible there is a binary file being tracked.
-      if last_modified_file != None:
-        # logging.info("last modified file: " + last_modified_file)
-        # logging.info(str(lines_modified_info.split(" ")))
-
-        # get the starting line of code that was modified
-        line_introduction = lines_modified_info.split(" ")[1][1:] # also remove the '-' in front of line #
-
-        # also remove the comma; we only use the start of the modification/deletion.
-        if (line_introduction.find(",") != -1):
-          line_introduction = line_introduction[0:line_introduction.find(",")]
-
-        region_diff[last_modified_file].append(line_introduction)
+          current_line-=1 # line numbers are negative -> so increment we subtract.
 
     return region_diff
+
 
   def getModifiedRegions(self, commit):
     """
@@ -178,6 +167,7 @@ class GitCommitLinker:
 
     # diff cmd w/ no lines of context between current vs parent
     diff_cmd = "git diff " + commit.commit_hash + "^ "+ commit.commit_hash + " --unified=0"
+    diff = str(subprocess.check_output(diff_cmd, shell=True))
 
     # files changed, this is used by the getLineNumbersChanged function
     diff_cmd_lines_changed = "git diff " + commit.commit_hash + "^ "+ commit.commit_hash + " --name-only"
@@ -187,7 +177,7 @@ class GitCommitLinker:
     files_modified = str( subprocess.check_output( diff_cmd_lines_changed, shell=True )).replace("b'", "").split("\\n")
 
     # now, let's get the file and the line number changed in the commit
-    return self._getModifiedRegions(diff_cmd, files_modified)
+    return self._getModifiedRegionsOnly(diff, files_modified)
 
 
   def gitAnnotate(self, regions, commit):
@@ -212,12 +202,12 @@ class GitCommitLinker:
         if line != 0 and line != "0" :
 
           # we need to git blame with the --follow option so that it follows renames in the file, and the '-l'
-          # option gives us the complete commit hash. additionally, start looking at the commit's direct ancestor.
+          # option gives us the complete commit hash. additionally, start looking at the commit's ancestor 
           buggy_change = str( subprocess.check_output( "git blame -L" + line + ",+1 " + commit.commit_hash + "^ -l -- " \
                             + file, shell=True )).split(" ")[0][2:]
 
           if buggy_change not in bug_introducing_changes:
-            logging.info("blamming " + buggy_change + " for line "+ str(line) + " in file " + file + " in commit " + commit.commit_hash)
+           # logging.info("blamming " + buggy_change + " for line "+ str(line) + " in file " + file + " in commit " + commit.commit_hash)
             bug_introducing_changes.append(buggy_change)
 
     return bug_introducing_changes
